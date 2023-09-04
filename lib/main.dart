1605ed0d86e1ba5254ef1896101a0e238d6594c4
DeployChef <dc.titovvlad@gmail.com>
2023-09-04 17:57:21 +0300

    add needle sprite

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 
@@ -6,9 +7,15 @@ void main() {
 }
 
 class MyGame extends FlameGame {
+  SpriteComponent needle = SpriteComponent();
   @override
   Future<void> onLoad() async {
     super.onLoad();
     print("we will load the game assets here");
+    needle
+      ..sprite = await loadSprite("needle.png")
+      ..size = Vector2(300, 300)
+      ..y = 100;
+    add(needle);
   }
 }
